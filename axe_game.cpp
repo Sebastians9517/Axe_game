dec0ba760a345f51093688994580c0d9140c9bd8
Sebastian <sebastians9517@gmail.com>
2023-08-09 02:21:37 -0500

    test

---
+++ axe_game.cpp
@@ -0,0 +1,12 @@
+#include <raylib.h>
+
+int main() {
+    // Window dimensions
+    int width = 350;
+    int height = 200;    
+    InitWindow(width, height, "Axe game");
+    // Circle position coordinates and radius
+    int circle_x = 175;
+    int circle_y = 100;
+    int circle_radius = 20;
+}
